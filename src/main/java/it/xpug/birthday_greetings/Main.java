048ab4de50ca51ee0a090a0a1c1e5cbaf6e1c379
Allen Vargas <allenbfox@gmail.com>
2020-05-11 21:33:15 -0400

    Refact main()

--- src/main/java/it/xpug/birthday_greetings/Main.java
+++ src/main/java/it/xpug/birthday_greetings/Main.java
@@ -9,13 +9,11 @@ import javax.mail.internet.*;
 public class Main {
 
 	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
-		BirthdayService service = new BirthdayService();
-		service.sendGreetings("employee_data.txt", new XDate(), "localhost", 25);
 		
-		/*/EmployeeRepository repository = new FileEmployeeRepostory("employee_data.txt");
-		EmailService emailService = new SmtpMailService("localhost", 25);
+		IEmployeeRepository repository = new FileEmployeeRepository("employee_data.txt");
+		IEmailService emailService = new SmtpMailService("localhost", 25);
 		BirthdayService service = new BirthdayService(repository, emailService);
-		service.sendGreetings(new XDate());/*/
+		service.sendGreetings(new XDate());
 	}
 
 }
