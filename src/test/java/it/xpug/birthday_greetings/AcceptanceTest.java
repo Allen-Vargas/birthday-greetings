5e5e2f57d8c932297548484028c76021061a3575
Allen Vargas <allenbfox@gmail.com>
2020-05-11 21:42:29 -0400

    Update AcceptanceTest

--- src/test/java/it/xpug/birthday_greetings/AcceptanceTest.java
+++ src/test/java/it/xpug/birthday_greetings/AcceptanceTest.java
@@ -19,7 +19,9 @@ public class AcceptanceTest {
 	@Before
 	public void setUp() throws Exception {
 		mailServer = SimpleSmtpServer.start(NONSTANDARD_PORT);
-		birthdayService = new BirthdayService();
+		IEmployeeRepository repository = new FileEmployeeRepository("employee_data.txt");
+		IEmailService emailService = new SmtpMailService("localhost", NONSTANDARD_PORT);
+		birthdayService = new BirthdayService(repository, emailService);
 	}
 
 	@After
@@ -31,7 +33,7 @@ public class AcceptanceTest {
 	@Test
 	public void willSendGreetings_whenItsSomebodysBirthday() throws Exception {
 
-		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/10/08"), "localhost", NONSTANDARD_PORT);
+		birthdayService.sendGreetings(new XDate("2008/10/08"));
 
 		assertEquals("message not sent?", 1, mailServer.getReceivedEmailSize());
 		SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmail().next();
@@ -44,7 +46,7 @@ public class AcceptanceTest {
 
 	@Test
 	public void willNotSendEmailsWhenNobodysBirthday() throws Exception {
-		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/01/01"), "localhost", NONSTANDARD_PORT);
+		birthdayService.sendGreetings(new XDate("2008/01/01"));
 
 		assertEquals("what? messages?", 0, mailServer.getReceivedEmailSize());
 	}
